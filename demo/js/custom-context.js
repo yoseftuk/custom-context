aea8c18ddbdc218e4025828a86acda2ba7b186c4
yoseftuk <yoostuk@gmail.com>
2019-12-23 09:29:45 +0200

    add drawImageCover and drawImageContain functions

--- demo/js/custom-context.js
+++ demo/js/custom-context.js
@@ -1,12 +1,11 @@
-
-HTMLCanvasElement.prototype.getCustomContext = function() {
+HTMLCanvasElement.prototype.getCustomContext = function () {
 
     const ctx = this.getContext('2d');
     const canvas = this;
-    if(!ctx) return null;
+    if (!ctx) return null;
 
     // -- DRAW ARROW -- //
-    ctx.drawArrow = function(x1, y1, x2, y2, arrowEndHeight = 10, arrowEndWidth = 6) {
+    ctx.drawArrow = function (x1, y1, x2, y2, arrowEndHeight = 10, arrowEndWidth = 6) {
         this.beginPath();
         this.moveTo(x1, y1);
         this.lineTo(x2, y2);
@@ -51,69 +50,136 @@ HTMLCanvasElement.prototype.getCustomContext = function() {
     };
 
     // -- LOAD IMAGE -- //
-    ctx.loadImage = function(url) {
-      return new Promise((resolve, reject) => {
-          const img = new Image();
-          img.src = url;
-          img.onload = () => {
-              img.onload = null;
-              resolve(img);
-          };
-          img.onerror = () => {
-              reject('field to load resource');
-          };
-      });
+    ctx.loadImage = function (url) {
+        return new Promise((resolve, reject) => {
+            const img = new Image();
+            img.src = url;
+            img.onload = () => {
+                img.onload = null;
+                resolve(img);
+            };
+            img.onerror = () => {
+                reject('field to load resource');
+            };
+        });
     };
 
     // -- DRAW IMAGE URL -- //
-    ctx.drawImageUrl = function(url, dx, dy, dw, dh, sx, sy, sw, sh) {
+    ctx.drawImageUrl = function (url, dx, dy, dw, dh, sx, sy, sw, sh) {
         this.loadImage(url)
             .then(img => {
-                if(sx) {
+                if (sx) {
                     this.drawImage(img, dx, dy, dw, dh, sx, sy, sw, sh);
-                } else if(dw) {
+                } else if (dw) {
                     this.drawImage(img, dx, dy, dw, dh)
                 } else {
                     this.drawImage(img, dx, dy)
                 }
             })
-            .catch(() => {});
+            .catch(() => {
+            });
     };
 
 
     // -- DRAW CIRCLE IMAGE -- //
-    ctx.drawCircleImage = function(img, x, y, radius, sx = 0, sy = 0, sw = img.width, sh = img.height) {
+    ctx.drawCircleImage = function (img, x, y, radius, sx = 0, sy = 0, sw = img.width, sh = img.height) {
         this.save();
         this.beginPath();
         this.arc(x + radius, y + radius, radius, 0, Math.PI * 2, true);
         this.closePath();
         this.clip();
 
-        this.drawImage(img, sx, sy, sw, sh, x, y, radius*2, radius*2);
+        this.drawImage(img, sx, sy, sw, sh, x, y, radius * 2, radius * 2);
         this.restore();
     };
 
     // -- DRAW CIRCLE IMAGE URL -- //
-    ctx.drawCircleImageUrl = function(url, x, y, radius, sx = 0, sy = 0, sw, sh) {
-      this.loadImage(url)
-          .then(img => {
-              this.drawCircleImage(img, x, y, radius, sx, sy, sw || img.width, sh || img.height);
-          })
-          .catch(() => {});
+    ctx.drawCircleImageUrl = function (url, x, y, radius, sx = 0, sy = 0, sw, sh) {
+        this.loadImage(url)
+            .then(img => {
+                this.drawCircleImage(img, x, y, radius, sx, sy, sw || img.width, sh || img.height);
+            })
+            .catch(() => {
+            });
+    };
+
+    // -- LOAD IMAGE COVER -- //
+    ctx.loadImageCover = function (url, w, h) {
+        return new Promise((resolve, reject) => {
+            this.loadImage(url).then(img => {
+                const canvas = document.createElement('canvas');
+                canvas.width = w;
+                canvas.height = h;
+                const context = canvas.getCustomContext();
+                context.drawImageCover(img, 0, 0, w, h);
+                this.loadImage(canvas.toDataURL()).then(resolve).catch(reject);
+            }).catch(err => console.error(err));
+        });
+    };
+
+    // -- DRAW IMAGE COVER -- //
+    ctx.drawImageCover = function (img, x, y, w, h) {
+        const imgRatio = img.width / img.height;
+        const canvasRatio = w / h;
+
+        if (imgRatio > canvasRatio) {
+            this.drawImage(img, (img.width - img.width * canvasRatio / imgRatio) / 2, 0, img.width * canvasRatio / imgRatio, img.height, x, y, w, h);
+        } else {
+            this.drawImage(img, 0, (img.height - img.height * imgRatio / canvasRatio) / 2, img.width, img.height * imgRatio / canvasRatio, x, y, w, h);
+        }
+    };
+    // -- DRAW IMAGE COVER URL --//
+    ctx.drawImageCoverUrl = function (url, x, y, w, h) {
+        return this.loadImage(url).then(img => {
+            this.drawImageCover(img, x, y, w, h);
+        });
+    };
+
+    // -- LOAD IMAGE CONTAIN -- //
+    ctx.loadImageContain = function(url, w, h) {
+        return new Promise((resolve, reject) => {
+            this.loadImage(url).then(img => {
+                const canvas = document.createElement('canvas');
+                canvas.width = w;
+                canvas.height = h;
+                const context = canvas.getCustomContext();
+                context.drawImageContain(img, 0, 0, w, h);
+                this.loadImage(canvas.toDataURL()).then(resolve).catch(reject);
+            }).catch(err => console.error(err));
+        });
+    };
+
+    // -- DRAW IMAGE CONTAIN -- //
+    ctx.drawImageContain = function (img, x, y, w, h) {
+        const imgRatio = img.width / img.height;
+        const canvasRatio = w / h;
+
+        if (imgRatio > canvasRatio) {
+            this.drawImage(img, x, y + (h - h * canvasRatio / imgRatio) / 2, w , h * canvasRatio / imgRatio);
+        } else {
+            this.drawImage(img, x + (w - w * imgRatio/ canvasRatio) / 2, y, w * imgRatio/ canvasRatio, h);
+        }
+    };
+
+    // -- DRAW IMAGE CONTAIN URL -- //
+    ctx.drawImageContainUrl = function(url, x, y, w, h) {
+        return this.loadImage(url).then(img => {
+            this.drawImageContain(img, x, y, w, h);
+        });
     };
 
     // -- LINE POLYGON -- //
-    ctx.linePolygon = function(cx, cy, radius, vertexesNum, rotate = 0) {
+    ctx.linePolygon = function (cx, cy, radius, vertexesNum, rotate = 0) {
 
         this.moveTo(cx + Math.cos(rotate) * radius, cy + Math.sin(rotate) * radius);
-        for (let i=1; i<=vertexesNum; i++) {
+        for (let i = 1; i <= vertexesNum; i++) {
             this.lineTo(cx + Math.cos(rotate + i / vertexesNum * Math.PI * 2) * radius,
-                cy + Math.sin(rotate + i / vertexesNum * Math.PI * 2) * radius );
+                cy + Math.sin(rotate + i / vertexesNum * Math.PI * 2) * radius);
         }
     };
 
     // -- STROKE POLYGON
-    ctx.strokePolygon = function(cx, cy, radius, vertexesNum, rotate = 0) {
+    ctx.strokePolygon = function (cx, cy, radius, vertexesNum, rotate = 0) {
         this.beginPath();
         this.linePolygon(cx, cy, radius, vertexesNum, rotate);
         this.stroke();
@@ -121,7 +187,7 @@ HTMLCanvasElement.prototype.getCustomContext = function() {
     };
 
     // -- FILL POLYGON
-    ctx.fillPolygon = function(cx, cy, radius, vertexesNum, rotate = 0) {
+    ctx.fillPolygon = function (cx, cy, radius, vertexesNum, rotate = 0) {
         this.beginPath();
         this.linePolygon(cx, cy, radius, vertexesNum, rotate);
         this.fill();
@@ -129,7 +195,7 @@ HTMLCanvasElement.prototype.getCustomContext = function() {
     };
 
     // -- FILTERS -- //
-    ctx.filterImage = function(img) {
+    ctx.filterImage = function (img) {
         return new ImageFilter(this, img);
     };
 
