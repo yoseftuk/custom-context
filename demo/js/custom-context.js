43a3ae17fa894cf8c0b7475a8e19009792ccf96b
yoseftuk <yoostuk@gmail.com>
2020-01-03 05:27:03 +0200

    add functions: * get index position (-return row and column from data index) * replace pixels by (- callback receive row and column and return new row and column for given pixel) * flipHorizontal * flip vertical

--- demo/js/custom-context.js
+++ demo/js/custom-context.js
@@ -14,6 +14,45 @@ HTMLCanvasElement.prototype.getCustomContext = function () {
     ctx.clearAllRect = function() {
         return this.clearRect(0, 0, this.canvas.width, this.canvas.height);
     };
+    // -- FLIP VERTICAL -- //
+    ctx.flipVertical = function() {
+        this.replacePixelsBy((row, col) => ({
+            newRow: this.canvas.height - row,
+            newCol: col
+        }));
+    };
+    // -- FLIP HORIZONTAL -- //
+    ctx.flipHorizontal = function() {
+        this.replacePixelsBy((row, col) => ({
+          newRow: row,
+          newCol: this.canvas.width - col
+        }));
+    };
+    // -- GET INDEX POSITION -- //
+    ctx.getIndexPosition = function(index) {
+        const col = index % this.canvas.width;
+        const row = (index - col) / this.canvas.width;
+        return {row, col};
+    };
+    // -- REPLACE PIXELS BY -- //
+    ctx.replacePixelsBy = function(callback) {
+        const imageData = this.getAllImageData();
+        const data = [...imageData.data];
+        for (let i=0; i<this.canvas.width*this.canvas.height; i++) {
+            const {row, col} = this.getIndexPosition(i);
+            const {newRow, newCol} = callback(row, col);
+            const j = this.canvas.width * newRow * 4 + newCol * 4;
+            const k = i * 4;
+            for (let l=0; l<4; l++) {
+                imageData.data[j + l] = data[k + l];
+            }
+        }
+        this.putImageData(imageData, 0, 0);
+    };
+    // -- CIRCLE LIGHT -- //
+    ctx.circleLight = function(radius = 0, brightness = 1.5, vagueness = .5) {
+
+    };
     // -- FILTERS -- //
     ctx.filterImage = function (img) {
         return new ImageFilter(this, img);
--- demo/js/hot-spot.js
+++ demo/js/hot-spot.js
@@ -13,7 +13,21 @@ function HotSpot(ctx, type, options) {
             this._listener = ctx.canvas.addEventListener('click', e => {
                 const x = e.x / ctx.canvas.offsetWidth * ctx.canvas.width;
                 const y = e.y / ctx.canvas.offsetHeight * ctx.canvas.height;
+                const vertexCount = options.vertexCount >=3 ? options.vertexCount : 3;
+                const rotate = options.rotate !== undefined ? options.rotate : 0;
+                const cx = options.cx, cy = options.cy;
+                // -- FIND THE ANGLE FROM THE CENTER OF THE POINT TO THE EVENT POINT
+                const theta = x > cx ? Math.atan((cy - y) / (cx - x)) : -Math.atan((cy - y) / (cx - x));
+                console.log(theta);
+                // -- FIND THE EDGE WHERE A LINE FROM THE CENTER TO THE EVENT SHOULD MEET
+
+                // -- CALCULATE THE FUNCTION OF THE EDGE LINE
+                // -- CALCULATE THE FUNCTION OF THE LINE GOING FROM THE CENTER TO THE EVENT POINT
+                // -- CALCULATE THE MEETING POINT
+                // -- SEE WHETHER THE MEETING POINT IS BEFORE THE EVENT (=EVENT OUT) OR AFTER (=EVENT IN)
             });
+            break;
+        case 'custom':
             break;
         default:
     }
