1ef22bd40714af2baa5670267b7ce39d13065c18
yoseftuk <yoostuk@gmail.com>
2020-02-01 21:12:53 +0700

    refactoring structure and prepare gulp for docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,25 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const babel = require("gulp-babel");
+const sass = require("gulp-sass");
 
 gulp.task('concatjs', () => (
-    gulp.src('demo/js/*.js')
+    gulp.src('src/*.js')
         .pipe(concat('custom-context.js'))
         .pipe(babel())
         .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+));
+gulp.task('docsjs', () => (
+    gulp.src('dist/src/*.js')
+        .pipe(concat('custom-context.js'))
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
+));
+gulp.task('docssass', () => (
+    gulp.src('docs/src/scss/index.scss')
+        .pipe(sass())
         .pipe(gulp.dest('dist'))
 ));
 
@@ -16,5 +29,7 @@ gulp.task('default', gulp.series([
 ]));
 
 gulp.task('watch', () => {
-    gulp.watch('demo/js/*.js', gulp.series(['concatjs']));
+    gulp.watch('src/*.js', gulp.series(['concatjs']));
+    gulp.watch('docs/src/js/*.js', gulp.series(['docsjs']));
+    gulp.watch('docs/src/scss/*.scss', gulp.series(['docssass']));
 });
